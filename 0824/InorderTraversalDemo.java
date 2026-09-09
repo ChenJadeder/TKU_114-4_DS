043472f279b77e81d3be9209e0622c5f59c66bdd
ChenJadeder <117714224+ChenJadeder@users.noreply.github.com>
2026-09-09 19:03:35 +0900

    update concept pracitice

---
+++ 0824/InorderTraversalDemo.java
@@ -0,0 +1,32 @@
+// 概念 9：Inorder traversal，Left-Root-Right
+class InorderNode {
+    int value;
+    InorderNode left;
+    InorderNode right;
+
+    InorderNode(int value) {
+        this.value = value;
+    }
+}
+
+public class InorderTraversalDemo {
+    static void inorder(InorderNode node) {
+        if (node == null) {
+            return;
+        }
+        inorder(node.left);
+        System.out.print(node.value + " ");
+        inorder(node.right);
+    }
+
+    public static void main(String[] args) {
+        InorderNode root = new InorderNode(40);
+        root.left = new InorderNode(10);
+        root.right = new InorderNode(30);
+        root.left.left = new InorderNode(20);
+
+        inorder(root);
+        System.out.println();
+        System.out.println("ordinary binary tree");
+    }
+}
