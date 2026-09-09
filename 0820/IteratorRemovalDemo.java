35a7c85c149d867e4354b81ee963a219f5f79701
ChenJadeder <117714224+ChenJadeder@users.noreply.github.com>
2026-09-09 16:56:34 +0900

    update concept  practice

---
+++ 0820/IteratorRemovalDemo.java
@@ -0,0 +1,25 @@
+// 概念 7：Iterator 與走訪中安全刪除
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class IteratorRemovalDemo {
+    public static void main(String[] args) {
+        List<Integer> scores = new ArrayList<>();
+        scores.add(12);
+        scores.add(35);
+        scores.add(8);
+        scores.add(50);
+
+        Iterator<Integer> iterator = scores.iterator();
+        while (iterator.hasNext()) {
+            int score = iterator.next();
+            if (score < 20) {
+                iterator.remove();
+            }
+        }
+
+        System.out.println(scores);
+    }
+}
