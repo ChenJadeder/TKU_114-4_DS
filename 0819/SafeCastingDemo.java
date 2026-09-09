7b093e2d4b1e0220005dc0937dd11cf5739e9d96
ChenJadeder <117714224+ChenJadeder@users.noreply.github.com>
2026-09-09 15:08:29 +0900

    Concept 9 : Upcasting、downcasting 與 instanceof
    Implement SafeCastingDemo with media asset classes

---
+++ 0819/SafeCastingDemo.java
@@ -0,0 +1,60 @@
+// 概念 9：Upcasting、downcasting 與 `instanceof`
+
+// 程式檔名：`SafeCastingDemo.java`
+
+abstract class MediaAsset {
+    private final String title;
+
+    MediaAsset(String title) {
+        this.title = title;
+    }
+
+    String getTitle() {
+        return title;
+    }
+
+    abstract void play();
+}
+
+class VideoAsset extends MediaAsset {
+    private String resolution;
+
+    VideoAsset(String title, String resolution) {
+        super(title);
+        this.resolution = resolution;
+    }
+
+    @Override
+    void play() {
+        System.out.println("Play video: " + getTitle() + " " + resolution);
+    }
+
+    void changeResolution(String resolution) {
+        this.resolution = resolution;
+    }
+}
+
+class AudioAsset extends MediaAsset {
+    AudioAsset(String title) {
+        super(title);
+    }
+
+    @Override
+    void play() {
+        System.out.println("Play audio: " + getTitle());
+    }
+}
+
+public class SafeCastingDemo {
+    static void prepare(MediaAsset asset) {
+        if (asset instanceof VideoAsset video) {
+            video.changeResolution("1080p");
+        }
+        asset.play();
+    }
+
+    public static void main(String[] args) {
+        prepare(new VideoAsset("Tree Tutorial", "720p"));
+        prepare(new AudioAsset("Queue Podcast"));
+    }
+}
